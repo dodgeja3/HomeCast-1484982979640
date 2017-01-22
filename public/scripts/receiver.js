44e70ab23308a0b91699ea05579c0ceec89354ac
Spencer Ottarson <speceottar@gmail.com>
2017-01-22 00:48:36 -0500

    Added weather to receiver

--- public/scripts/receiver.js
+++ public/scripts/receiver.js
@@ -16,7 +16,6 @@ $(document).ready(function () {
                 
             }, "json");
         } else if (json.action == "update") {
-            document.getElementById("test").innerHTML=json.action;
             
             $(".widget").remove();
             $.get( "/api/widgets", function( data ) {
@@ -30,11 +29,26 @@ $(document).ready(function () {
                         "</div>"
                     );
 
+                    if (doc.type == "weather") {
+                        var description = "<![CDATA[<img src=\"http://l.yimg.com/a/i/us/we/52/29.gif\"/>\n<BR />\n<b>Current Conditions:</b>\n<BR />Partly Cloudy\n<BR />\n<BR />\n<b>Forecast:</b>\n<BR /> Sat - Partly Cloudy. High: 55Low: 40\n<BR /> Sun - Cloudy. High: 48Low: 40\n<BR /> Mon - Cloudy. High: 43Low: 38\n<BR /> Tue - Mostly Cloudy. High: 42Low: 33\n<BR /> Wed - Rain. High: 42Low: 37\n<BR />\n<BR />\n<a href=\"http://us.rd.yahoo.com/dailynews/rss/weather/Country__Country/*https://weather.yahoo.com/country/state/city-2436453/\">Full Forecast at Yahoo! Weather</a>\n<BR />\n<BR />\n(provided by <a href=\"http://www.weather.com\" >The Weather Channel</a>)\n<BR />\n]]>";
+                        $('.weather > .widget_content').html(
+                            "<span>" + description + "</span>"
+                        );
+
+                        //$.get( "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22lansing%2C%20mi%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys", function( data ) {
+                        //    //var description = data.query.results.channel.item.description;
+                        //    var description = "<![CDATA[<img src=\"http://l.yimg.com/a/i/us/we/52/29.gif\"/>\n<BR />\n<b>Current Conditions:</b>\n<BR />Partly Cloudy\n<BR />\n<BR />\n<b>Forecast:</b>\n<BR /> Sat - Partly Cloudy. High: 55Low: 40\n<BR /> Sun - Cloudy. High: 48Low: 40\n<BR /> Mon - Cloudy. High: 43Low: 38\n<BR /> Tue - Mostly Cloudy. High: 42Low: 33\n<BR /> Wed - Rain. High: 42Low: 37\n<BR />\n<BR />\n<a href=\"http://us.rd.yahoo.com/dailynews/rss/weather/Country__Country/*https://weather.yahoo.com/country/state/city-2436453/\">Full Forecast at Yahoo! Weather</a>\n<BR />\n<BR />\n(provided by <a href=\"http://www.weather.com\" >The Weather Channel</a>)\n<BR />\n]]>";
+                        //    $('.weather > .widget_content').html(
+                        //        "<span>" + description + "</span>"
+                        //    );
+                        //});
+                    }
+
                     var widget = $("#" + doc._id);
                     widget.css("height", ((parseFloat(doc.y2) - parseFloat(doc.y1))*100) + "%");
                     widget.css("width", ((parseFloat(doc.x2) - parseFloat(doc.x1))*100) + "%");
                     widget.css( "left", (parseFloat(doc.x1)*100) + "%");
-                    widget.css( "top", (parseFloat(doc.y)*100) + "%");
+                    widget.css( "top", (parseFloat(doc.y1)*100) + "%");
                     //$("#" + doc._id + " > .widget_content").css("line-height", (widget.height() - 50) + "px");
                 });
             });
